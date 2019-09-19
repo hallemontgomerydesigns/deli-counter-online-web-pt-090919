a29f8d5cfc2c77cff231f20c26d7dbc432e594db
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-19 01:54:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,12 +5,12 @@ katz_deli=[]
 other_deli= ["Logan", "Avi", "Spencer"]
 
 another_deli=["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
-def line(deli)
+def line(katz_deli)
   if deli.empty?
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, i|
+  katz_deli.each.with_index(1) do |person, i|
       current_line << " #{i}. #{person}"
     end
     puts current_line
